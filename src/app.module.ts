9b3e4ae9184efa9fc1db40c756b397b54e1cd92a
Elias Koivuporras <eppu2002@gmail.com>
2023-06-26 16:14:41 +0300

    add track entity and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from 'ormconfig'
 import { DriverModule } from './driver/driver.module';
 import { CarModule } from './car/car.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot(AppDataSource.options),
     DriverModule,
-    CarModule
+    CarModule,
+    TrackModule
   ],
   controllers: [AppController],
   providers: [AppService],
